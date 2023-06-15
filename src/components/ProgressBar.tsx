3747187e1026ba33ea5e423ab704bb0af0827ab2
benben <z2266109@gmail.com>
2023-06-15 23:33:59 +0800

    fix: invalid value

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import type { ChangeEvent } from 'react'
 
 function App() {
-  const [progressLength, setProgressLength] = useState(10)
-  const [value, setValue] = useState(20)
+  const [progressLength, setProgressLength] = useState(15)
+  const [totalValue, setTotalValue] = useState(100)
+  const [currentValue, setCurrentValue] = useState(20)
   const [startChar, setStartChar] = useState('★')
   const [endChar, setEndChar] = useState('✩')
 
@@ -12,35 +13,52 @@ function App() {
     if (!value || +value === 0) return
     setProgressLength(+value)
   }
-  const updateValue = (e: ChangeEvent<HTMLInputElement>) => setValue(+e.target.value)
-  const updateStartChar = (e: ChangeEvent<HTMLInputElement>) => setStartChar(e.target.value.slice(-1))
-  const updateEndChar = (e: ChangeEvent<HTMLInputElement>) => setEndChar(e.target.value.slice(-1))
+  const updateCurrentValue = (e: ChangeEvent<HTMLInputElement>) => setCurrentValue(+e.target.value)
+  const updateTotalValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    if (!value || +value === 0) return
+    setTotalValue(+value)
+  }
+  const updateStartChar = (e: ChangeEvent<HTMLInputElement>) => setStartChar(e.target.value)
+  const updateEndChar = (e: ChangeEvent<HTMLInputElement>) => setEndChar(e.target.value)
+
+  useEffect(() => {
+    setCurrentValue(0)
+  }, [totalValue])
 
   function mapValueToPregress() {
-    const startCount = Math.floor(value / progressLength)
-    return `${Array(startCount).fill(startChar).join('').padEnd(progressLength, endChar)}`
+    const startCount = Math.floor((currentValue / totalValue) * progressLength)
+    const endCount = progressLength - startCount
+    return `${Array(startCount).fill(startChar).join('')}${Array(endCount).fill(endChar).join('')}`
   }
 
-  const showValue = () => `${value} %`
+  const showValue = () => `${Math.floor((currentValue / totalValue) * 100)} %`
 
   return (
     <>
       <h3>Progress bar</h3>
       <div className="card">
-        <label>Range: {progressLength} </label>
+        <label>Total value: </label>
+        <input type="text" value={totalValue} onChange={updateTotalValue} />
+      </div>
+      <div className="card">
+        <label>Current value: </label>
+        <input type="range" max={totalValue} min="0" step="1" value={currentValue} onChange={updateCurrentValue} />
+        <span>{currentValue}</span>
+      </div>
+      <div className="card">
+        <div>
+          <label>Start Char: </label>
+          <input type="text" value={startChar} onChange={updateStartChar} />
+        </div>
+        <div>
+          <label>End Char: </label>
+          <input type="text" value={endChar} onChange={updateEndChar} />
+        </div>
+      </div>
+      <div className="card">
+        <label>Progress length: </label>
         <input type="number" value={progressLength} onChange={updateProgressLength} />
-      </div>
-      <div className="card">
-        <label>Value: {value} </label>
-        <input type="range" max="100" min="0" step="1" value={value} onChange={updateValue} />
-      </div>
-      <div className="card">
-        <label>Start: </label>
-        <input type="text" value={startChar} onChange={updateStartChar} />
-      </div>
-      <div className="card">
-        <label>End: </label>
-        <input type="text" value={endChar} onChange={updateEndChar} />
       </div>
       <div className="card">
         <div>Output</div>
