dad1c0df9cf19352505b0ddb2f7c85b412ece3b1
Peter <pittpeterliao@gmail.com>
2023-06-19 20:39:41 +0800

    feat: simplify copy call toast

--- src/hooks/useCopy.ts
+++ src/hooks/useCopy.ts
@@ -1,19 +1,13 @@
+import { SimpleToast } from '../components/Toast'
+
 export const useCopy = () => {
   const copyContent = async (text: string) => {
-    const toastSuccess = document.querySelector('#notify-alert-copySuccess')
-    const toastDanger = document.querySelector('#notify-alert-copyError')
     try {
       await navigator.clipboard.writeText(text)
-      toastSuccess?.classList.remove('hidden')
-      setTimeout(function () {
-        toastSuccess?.classList.add('hidden')
-      }, 2000)
+      SimpleToast.success('notify.alert.copySuccess')
       console.log('Content copied to clipboard')
     } catch (err) {
-      toastDanger?.classList.remove('hidden')
-      setTimeout(function () {
-        toastDanger?.classList.add('hidden')
-      }, 2000)
+      SimpleToast.error('notify.alert.copyError')
       console.error('Failed to copy: ', err)
     }
   }
