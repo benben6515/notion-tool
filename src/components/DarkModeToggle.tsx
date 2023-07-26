58b51bfa4149a6c97af2adef74a64e95291f7224
Peter <pittpeterliao@gmail.com>
2023-07-27 01:02:05 +0800

    fix: dark mode wrong icon

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Button } from 'flowbite-react'
 
 function DarkModeToggle() {
@@ -8,6 +8,13 @@ function DarkModeToggle() {
     (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
   const [isDarkMode, setIsDarkMode] = useState(isDarkSystemNow)
   const [hasClicked, setHasClicked] = useState(false)
+
+  useEffect(() => {
+    if (isDarkSystemNow) {
+      document.documentElement.classList.add('dark')
+      document.querySelector('#dark-mode-icon')?.classList.add('rotate-180')
+    }
+  }, [])
 
   const updateIsDarkMode = () => {
     isDarkMode ? document.documentElement.classList.remove('dark') : document.documentElement.classList.add('dark')
@@ -22,7 +29,10 @@ function DarkModeToggle() {
       <section className="flex justify-center p-2 w-24">
         <Button className="overflow-y-hidden" color="gray" onClick={updateIsDarkMode}>
           <div className="h-[16px]">
-            <div className={hasClicked ? (isDarkMode ? 'animate-dark-mode' : 'animate-dark-mode-continue') : ''}>
+            <div
+              id="dark-mode-icon"
+              className={hasClicked ? (isDarkMode ? 'animate-dark-mode' : 'animate-dark-mode-continue') : ''}
+            >
               <svg
                 className="w-5 h-5 text-gray-500 mb-[28px]"
                 aria-hidden="true"
